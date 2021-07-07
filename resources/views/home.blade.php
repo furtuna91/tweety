3bb590731b6bd7845ace14415bd1c9a087d95775
Furtuna Virgil <virgil.furtuna@gmail.com>
2021-07-07 19:31:31 +0300

    Design the timeline

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,19 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
+    <div class="md:flex md:justify-between">
+        <div class="md:w-1/6">
+            @include('sidebar-links')
+        </div>
+        <div class="md:flex-1 md:mx-10">
+            @include('publish-tweet-panel')
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
+            <div class="border border-gray-300 rounded-lg">
+                @include('tweet')
             </div>
         </div>
+        <div class="md:w-1/6 bg-blue-100 rounded-lg p-4">
+            @include('friends-list')
+        </div>
     </div>
-</div>
 @endsection
---
+++ resources/views/tweet.blade.php
@@ -0,0 +1,9 @@
+<div class="flex p-4 border-b border-b-gray-400">
+    <div class="mr-4 flex-shrink-0">
+        <img src="https://i.pravatar.cc/50" alt="" class="rounded-full mr-2">
+    </div>
+    <div>
+        <h5 class="font-bold mr-2">John Doe</h5>
+        <p class="text-sm">Lorem ipsum</p>
+    </div>
+</div>
