6a23b23ad0511270bb661f194e60054ccd96b4f6
Furtuna Virgil <virgil.furtuna@gmail.com>
2021-07-13 10:10:27 +0300

    Build a following

--- resources/views/friends-list.blade.php
+++ resources/views/friends-list.blade.php
@@ -1,8 +1,11 @@
-<h3 class="font-bold text-xl mb-4">Friends</h3>
+<h3 class="font-bold text-xl mb-4">Following</h3>
 <ul>
-    @foreach (range(1,8) as $item)
+    @foreach (auth()->user()->follows as $user)
         <li class="mb-4">
-            <div class="flex items-center text-sm"><img src="https://i.pravatar.cc/40" alt="" class="rounded-full mr-2"> John Doe</div>
+            <div class="flex items-center text-sm">
+                <img src="{{ $user->avatar }}" alt="" class="rounded-full mr-2">
+                {{ $user->name }}
+            </div>
         </li>
     @endforeach
 </ul>
